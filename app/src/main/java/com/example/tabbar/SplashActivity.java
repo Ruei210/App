824b988c3a625ce2ddaeaea93d71ebad70f843fe
Zoe <zoe21750000@gmail.com>
2020-12-30 22:27:17 +0800

    splash screen

---
+++ app/src/main/java/com/example/tabbar/SplashActivity.java
@@ -0,0 +1,26 @@
+package com.example.tabbar;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+public class SplashActivity extends Activity {
+    private ImageView imageView;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        imageView = findViewById(R.id.img_welcome);
+        imageView.setImageResource(R.drawable.welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        },3000);
+    }
+}

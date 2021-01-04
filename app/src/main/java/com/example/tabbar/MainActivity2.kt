5a1c50158efe2dcfb01692ac649859d71f5a75da
Ruei210 <anninelee111@gmail.com>
2021-01-04 21:26:28 +0800

    new(calendar)

---
+++ app/src/main/java/com/example/tabbar/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.tabbar
+
+import android.app.DatePickerDialog
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
+
+class MainActivity2 : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_home)
+
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+
+
+
+    }
+}

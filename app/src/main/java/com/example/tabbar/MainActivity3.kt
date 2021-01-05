445aa2eed7874a9a41ffb99883f3f62097b2035c
Ruei210 <anninelee111@gmail.com>
2021-01-05 21:27:27 +0800

    new(主頁)

--- app/src/main/java/com/example/tabbar/MainActivity3.kt
+++ app/src/main/java/com/example/tabbar/MainActivity3.kt
@@ -1,11 +1,17 @@
 package com.example.tabbar
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 
 class MainActivity3 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_yogamountain)
+        setContentView(R.layout.fragment_home)
+
     }
 }

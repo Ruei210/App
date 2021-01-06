2779799a0c08c7b822a2167dbda50a0b6d8105bc
Linda <lindasandy2835@gmail.com>
2021-01-07 00:18:36 +0800

    food 版2

---
+++ app/src/main/java/com/example/tabbar/berryOatsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tabbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class berryOatsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_berry_oats)
+    }
+}
--- app/src/main/java/com/example/tabbar/fragments/FoodFragment.kt
+++ app/src/main/java/com/example/tabbar/fragments/FoodFragment.kt
@@ -1,11 +1,15 @@
 package com.example.tabbar.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import com.example.tabbar.R
+import com.example.tabbar.berryOatsActivity
+import com.example.tabbar.itasaladActivity
 
 
 class FoodFragment : Fragment() {
@@ -24,5 +28,17 @@ class FoodFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_food, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<ImageButton>(R.id.itaSalad_imgbt).setOnClickListener {
+            val i = Intent(activity,itasaladActivity::class.java)
+            activity?.startActivity(i)
+        }
+        view.findViewById<ImageButton>(R.id.berryOats_imgbt).setOnClickListener {
+            val i = Intent(activity,berryOatsActivity::class.java)
+            activity?.startActivity(i)
+        }
+    }
 
 }
